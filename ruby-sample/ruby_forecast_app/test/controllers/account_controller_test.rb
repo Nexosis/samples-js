c32443772ad2845c37c2c76a068a7dda2f900ed5
Jeff Gabriel <jeffgabriel@outlook.com>
2017-06-26 16:20:34 -0400

    moved ruby-sample to own repo

--- ruby-sample/ruby_forecast_app/test/controllers/account_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class AccountControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get account_index_url
-    assert_response :success
-  end
-
-end
--- ruby-sample/ruby_forecast_app/test/controllers/results_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ResultsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get results_index_url
-    assert_response :success
-  end
-
-end
